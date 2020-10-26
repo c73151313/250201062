a75d45841e344681f19a9a959808c92da6bc3c4e
c73151313 <rahimezeynepgurer@gmail.com>
2020-10-26 08:38:36 +0000

    What did you change?

---
+++ lab1/exercise1.py
@@ -0,0 +1,4 @@
+animals = 10
+chickens = 6
+rooster = (animals - chickens)
+print ("There are", rooster, "roosters")
---
+++ lab2 /absolute.py
@@ -0,0 +1,7 @@
+#absolute value
+number = float(input("Enter a number"))
+if number < 0:
+  print ((-1)*(number))
+  else:
+    print (number)
+    
